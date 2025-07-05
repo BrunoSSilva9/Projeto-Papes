9d9097c7664fb01ddbe253dc08b53d8a5e6f1610
Andre-Santos-de-Souza <160162720+AndreSantosDS@users.noreply.github.com>
2025-07-05 16:42:44 -0300

    Implementa envio do formulário para JSON Server com async/await

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,15 +1,51 @@
-    const menuButton = document.getElementById('menuButton');
-    const dropdownMenu = document.getElementById('dropdownMenu');
+const menuButton = document.getElementById("menuButton");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
-    menuButton.addEventListener('click', function(event) {
-        event.stopPropagation();
-        
+menuButton.addEventListener("click", function (event) {
+  event.stopPropagation();
 
-        dropdownMenu.classList.toggle('active');
+  dropdownMenu.classList.toggle("active");
+});
+
+window.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("active")) {
+    dropdownMenu.classList.remove("active");
+  }
+});
+
+// Código para envio do formulário com async/await
+const formulario = document.getElementById("inscricaoForm");
+
+formulario.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const dadosFormulario = {
+    nome: formulario.nome.value,
+    email: formulario.email.value,
+    telefone: formulario.telefone.value,
+    curso: formulario.curso.value,
+    dataNascimento: formulario["data-nascimento"].value,
+    matricula: formulario.matricula.value,
+    motivo: formulario.motivo.value,
+  };
+
+  try {
+    const response = await fetch("http://localhost:3001/formularios", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dadosFormulario),
     });
 
-    window.addEventListener('click', function() {
-        if (dropdownMenu.classList.contains('active')) {
-            dropdownMenu.classList.remove('active');
-        }
-    });
+    if (response.ok) {
+      alert("Formulário enviado com sucesso!");
+      formulario.reset();
+    } else {
+      alert("Erro ao enviar o formulário.");
+    }
+  } catch (error) {
+    console.error("Erro:", error);
+    alert("Erro na comunicação com o servidor.");
+  }
+});
